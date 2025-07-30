2869fd990c692d2046f95bc3d67b76e3b198a8a8
Sebalos14 <sebastianpalacios1401@gmail.com>
2025-07-29 21:54:57 -0600

    Update AutoBus.java
    Creando clase hija autobus e introduciendo codigo

--- Transporte_Sistema/src/Transporte/AutoBus.java
+++ Transporte_Sistema/src/Transporte/AutoBus.java
@@ -1,4 +1,21 @@
 package Transporte;
 
-public class AutoBus {
+public class AutoBus extends Vehiculo {
+    private String rutaAsignada;
+    private int numeroPisos;
+
+    public AutoBus(String id, int capacidadPasajeros, float velocidadMaxima, String estado,
+                   String rutaAsignada, int numeroPisos) {
+        super(id, capacidadPasajeros, velocidadMaxima, estado);
+        this.rutaAsignada = rutaAsignada;
+        this.numeroPisos = numeroPisos;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.println("Ruta asignada: " + rutaAsignada);
+        System.out.println("Número de pisos: " + numeroPisos);
+        System.out.println("------------------------------");
+    }
 }
